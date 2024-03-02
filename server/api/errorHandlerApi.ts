16fd9d6393be2867ce3bb3f3c059aff3ee46ba64
Afranio Dantas <afraniodantas0224@gmail.com>
2024-03-02 12:10:13 -0300

    errorHandlesApi inplemented

--- server/api/errorHandlerApi.ts
+++ server/api/errorHandlerApi.ts
@@ -1,5 +1,19 @@
+import { Request, Response, ErrorRequestHandler, NextFunction } from "express";
 
-console.log('add')
+//criar a função de errorhanddler
+// a função recebe o erro, e retorna a mensagem de erro
+// adicionando as devidas propriedades
+export function errorHanddlerApi(err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction){
+    console.error(`API error handdler foi executada: ${err}`);
+    // para quem for receber o erro no navegador irá receber o erro 500
+    res.status(500).json({
+        errorCode: 'ERR-001',
+        message: 'Erro interno do servidor'
+    });
+}
+
+
+
 
 
 
